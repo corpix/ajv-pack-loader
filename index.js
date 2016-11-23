03cd6c9c123c759e3f05928beea950ab3a8bd87a
Dmitry Moskowski <me+ajv-pack-loader@corpix.ru>
2016-11-23 16:00:04 +0000

    Added query support

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 const Ajv = require('ajv');
 const pack = require('ajv-pack');
+const qs = require('querystring');
 const requireFromString = require('require-from-string');
 
 module.exports = function(source, sourceMap) {
     this.cacheable();
 
+    let query = JSON.parse(this.query.substr(1));
+    query.sourceCode = true;
+
     const callback = this.async();
-    const ajv = new Ajv({ sourceCode: true });
+    const ajv = new Ajv(query);
     let schema = ajv.compile(requireFromString(source));
 
     callback(
